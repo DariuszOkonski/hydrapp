da67ca32c1d69c8d35e833a93ca538155c3f4827
Dariusz Okonski <darek200180@gmail.com>
2020-06-23 13:02:37 +0100

    add js logic

--- src/js/index.js
+++ src/js/index.js
@@ -6,4 +6,32 @@ import '../scss/main.scss';
 
 /* place your code below */
 
-console.log('HELLO 🚀')
+const glassCounter = document.querySelector('.glass__counter--js');
+const buttonAdd = document.querySelector('.button-add--js');
+const buttonRemove = document.querySelector('.button-remove--js');
+const key = new Date().toLocaleString().slice(0, 10);
+
+const localStorageValue = localStorage.getItem(key);
+let currentGlassCounter = 0;
+
+if(localStorageValue) {
+    currentGlassCounter = localStorageValue;
+} else {
+    localStorage.setItem(key, 0);
+}
+
+glassCounter.innerHTML = currentGlassCounter;
+
+buttonAdd.addEventListener('click', () => {
+    currentGlassCounter++;
+    glassCounter.innerHTML = currentGlassCounter;
+    localStorage.setItem(key, currentGlassCounter);
+});
+
+buttonRemove.addEventListener('click', () => {
+    if(currentGlassCounter > 0) {
+        currentGlassCounter--
+    }
+    glassCounter.innerHTML = currentGlassCounter;
+    localStorage.setItem(key, currentGlassCounter);
+});
